b1b4e2f889961f4cd0b5a1f280a819a4429e9fef
Gaurav0321 <gauravawale2119@gmail.com>
2024-08-14 00:38:41 +0530

    Update index.js
    changes updated to js

--- Stone Paper Scissors/index.js
+++ Stone Paper Scissors/index.js
@@ -14,31 +14,31 @@ if(randomNumber >=0 && randomNumber<1){
 }
 if(cpuChoice === 'Paper' && yourChoice === 'Paper'){
     result=`Computer choice is ${cpuChoice} and you are choice is ${yourChoice} TiE!!`
-    alert('result')
+    alert(result)
 }else if(cpuChoice === 'Paper' && yourChoice === 'Stone'){
     result=`Computer choice is ${cpuChoice} and you are choice is ${yourChoice} Oops! you lose`
-    alert('result')
+    alert(result)
 }else if(cpuChoice === 'Paper' && yourChoice === 'Scissors'){
     result=`Computer choice is ${cpuChoice} and you are choice is ${yourChoice} Hurray! you WIN`
-    alert('result')
+    alert(result)
 }else if(cpuChoice === 'Stone' && yourChoice === 'Paper'){
      result=`Computer choice is ${cpuChoice} and you are choice is ${yourChoice} Hurray! you WIN`
-    alert('result')
+    alert(result)
 }else if(cpuChoice === 'Stone' && yourChoice === 'Stone'){
     result=`Computer choice is ${cpuChoice} and you are choice is ${yourChoice} TiE!!`
-    alert('result')
+    alert(result)
 }else if(cpuChoice === 'Stone' && yourChoice === 'Scissors'){
     result=`Computer choice is ${cpuChoice} and you are choice is ${yourChoice} Oops! you lose`
-    alert('result')
+    alert(result)
 }else if(cpuChoice === 'Scissors' && yourChoice === 'Paper'){
     result=`Computer choice is ${cpuChoice} and you are choice is ${yourChoice} Oops! you lose`
-    alert('result')
+    alert(result)
 }else if(cpuChoice === 'Scissors' && yourChoice === 'Stone'){
      result=`Computer choice is ${cpuChoice} and you are choice is ${yourChoice} Hurray! you WIN`
-    alert('result')
+    alert(result)
 }else if(cpuChoice === 'Scissors' && yourChoice === 'Scissors'){
     result=`Computer choice is ${cpuChoice} and you are choice is ${yourChoice} TiE!!`
-    alert('result')
+    alert(result)
 }else{
     alert('Mindblowing Code!!!')
 }
